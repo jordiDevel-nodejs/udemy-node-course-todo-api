dfea1075b6f6bedff1d15c9ba22a7bb08b0c7ad9
jmedina <skeptofeina@gmail.com>
2019-01-22 15:28:42 +0100

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://192.168.99.100:32768/TodoApp'
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://192.168.99.100:32768/TodoAppTest'
-}
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json');
+    const envConfig = config[env];
 
-module.exports = { env };
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
